f03928056e5385f02b7f7f9fc99cd4ae6db5a67d
Thiago Costa <tigurrock@gmail.com>
2023-03-03 10:52:09 -0300

    att

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById('game-canvas')
 const ctx = canvas.getContext("2d")
+// we will need the gamecontainer to make it blurry
+// when we display the end menu
 const gameContainer = document.getElementById('game-container')
 const flappyImg = new Image()
 flappyImg.src = 'assets/flappy_dunk.png'
@@ -24,31 +26,36 @@ let highScore = 0
 
 let scored = false
 
-document.body.onkeyup = function(e){
-    if(e.code == 'Space'){
+// lets us control the bird with the space key
+document.body.onkeyup = function(e) {
+    if (e.code == 'Space'){
         birdVelocity = FLAP_SPEED
     }
 }
 
+// lets us restart the game if we hit game-over
 document.getElementById('restart-button').addEventListener('click', function() {
     hideEndMenu()
     resetGame()
     loop()
 })
-function increaseScore(){
-    if  (birdX > pipeX + PIPE_WIDTH && 
-        (birdY < pipeY + PIPE_GAP || 
-        birdY + BIRD_HEIGHT > pipeY + PIPE_GAP) &&
-        !scored) {
-            score++
-            scoreDiv.innerHTML = score
-            scored = true
+
+function increaseScore() {
+    if(birdX > pipeX + PIPE_WIDTH &&
+        (birdY < pipeY + PIPE_GAP ||
+            birdY + BIRD_HEIGHT > pipeY + PIPE_GAP) &&
+            !scored) {
+        score++
+        scoreDiv.innerHTML = score
+        scored = true
     }
-    if(birdX < pipeX + PIPE_WIDTH) {
+    // reset the flag, if bird passes the pipes
+    if (birdX < pipeX + PIPE_WIDTH) {
         scored = false
     }
 }
-function collisionCheck(){
+function collisionCheck() {
+    // Create bounding Boxes for the bird and the pipes
     const birdBox = {
         x: birdX,
         y: birdY,
@@ -67,26 +74,29 @@ function collisionCheck(){
         width: PIPE_WIDTH,
         height: canvas.height - pipeY - PIPE_GAP
     }
+    // Check for collision with upper pipe box
     if (birdBox.x + birdBox.width > topPipeBox.x &&
         birdBox.x < topPipeBox.x + topPipeBox.width &&
         birdBox.y < topPipeBox.y) {
             return true
     }
+    // Check for collision with lower pipe box
     if (birdBox.x + birdBox.width > bottomPipeBox.x &&
         birdBox.x < bottomPipeBox.x + bottomPipeBox.width &&
         birdBox.y + birdBox.height > bottomPipeBox.y) {
             return true
     }
+    // check if bird hits boundaries
     if (birdY < 0 || birdY + BIRD_HEIGHT > canvas.height) {
         return true
     }
     return false
 }
-function hideEndMenu(){
+function hideEndMenu() {
     document.getElementById('end-menu').style.display = 'none'
     gameContainer.classList.remove('backdrop-blur')
 }
-function showEndMenu(){
+function showEndMenu() {
     document.getElementById('end-menu').style.display = 'block'
     gameContainer.classList.add('backdrop-blur')
     document.getElementById('end-score').innerHTML = score
@@ -95,36 +105,48 @@ function showEndMenu(){
     }
     document.getElementById('best-score').innerHTML = highScore
 }
-function resetGame(){
-    birdX = 50
-    birdY = 50
-    birdVelocity = 0
-    birdAcceleration = 0.1
+function resetGame() {
+    birdX = 50;
+    birdY = 50;
+    birdVelocity = 0;
+    birdAcceleration = 0.1;
 
-    pipeX = 400
-    pipeY = canvas.height - 200
-    score = 0
+    pipeX = 400;
+    pipeY = canvas.height - 200;
+
+    score = 0;
     scoreDiv.innerHTML = 0
 }
-function endGame(){
+function endGame() {
     showEndMenu()
 }
-function loop(){
+function loop() {
+    // reset the ctx after every loop iteration
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // Draw Flappy Bird
     ctx.drawImage(flappyImg, birdX, birdY)
+
+    // Draw Pipes
     ctx.fillStyle = '#333'
     ctx.fillRect(pipeX, -100, PIPE_WIDTH, pipeY)
     ctx.fillRect(pipeX, pipeY + PIPE_GAP, PIPE_WIDTH, canvas.height - pipeY)
 
+    // now we would need to add an collision check to display our end-menu
+    // and end the game
+    // the collisionCheck will return us true if we have a collision
+    // otherwise false
     if (collisionCheck()) {
         endGame()
         return
     }
+    // forgot to move the pipes
     pipeX -= 1.5
+    // if the pipe moves out of the frame we need to reset the pipe
     if (pipeX < -50){
         pipeX = 400
         pipeY = Math.random() * (canvas.height - PIPE_GAP) + PIPE_WIDTH
     }
+    // apply gravity to the bird and let it move
     birdVelocity += birdAcceleration
     birdY += birdVelocity
 
